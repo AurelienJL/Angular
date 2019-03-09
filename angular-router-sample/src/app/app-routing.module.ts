8cf74e44111b1bdec01a83a5d741d7d270ea80e0
AurelienJL <aurelienjl@live.fr>
2019-03-09 11:22:26 +0100

    test

---
+++ angular-router-sample/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroListComponent},
+  { path: '', redirectTo: '/heroes', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, {enableTracing: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
