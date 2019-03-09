7b92f2b86d443d92f6194f52d781a2982fd66f2f
AurelienJL <aurelienjl@live.fr>
2019-03-09 11:19:32 +0100

    observables added

--- ShoppingRecipeApp/src/app/recipes/recipe.service.ts
+++ ShoppingRecipeApp/src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
@@ -16,8 +16,6 @@ export class RecipeService {
             new Ingredient('Buns', 1), new Ingredient('Beef patty', 1), new Ingredient('Tomato', 3)
         ])
       ];
-
-    recipeSelected = new EventEmitter<Recipe>();
 
     constructor(private shoppingListService: ShoppingListService) {}
 
